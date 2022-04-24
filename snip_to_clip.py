d6ae599706a6a4031ab215e63dcba34dcedccedd
Abu-AM <abu.mohamed49@gmail.com>
2022-04-24 01:42:24 -0600

    Cleaned up code, added credit

--- snip_to_clip.py
+++ snip_to_clip.py
@@ -1,8 +1,11 @@
-from tkinter import *
-import datetime
-import pyautogui
-from PIL import Image
+"""
+Snipping & saving implementation taken from: https://stackoverflow.com/a/61603758
+"""
+
 import subprocess
+from tkinter import Toplevel, Frame, Canvas, Tk, BOTH, YES
+from PIL import Image
+import pyautogui
 import pytesseract
 
 
@@ -17,33 +20,18 @@ class Application:
         self.curY = None
         self.img = None
 
-        # root.configure(background = 'red')
-        # root.attributes("-transparentcolor","red")
-
         root.withdraw()
-        # root.attributes("-transparent", "blue")
-        # root.geometry("400x50+200+200")  # set new geometry
-        # root.title("SniptoClip")
-        # self.menu_frame = Frame(master, bg="blue")
-        # self.menu_frame.pack(fill=BOTH, expand=YES)
-
-        # self.buttonBar = Frame(self.menu_frame, bg="")
-        # self.buttonBar.pack(fill=BOTH, expand=YES)
-
-        # self.snipButton = Button(
-        #     self.buttonBar, width=3, command=self.createScreenCanvas, background="green"
-        # )
-        # self.snipButton.pack(expand=YES)
 
         self.master_screen = Toplevel(root)
         self.master_screen.withdraw()
         self.master_screen.attributes("-transparent", "blue")
+
         self.picture_frame = Frame(self.master_screen, background="blue")
         self.picture_frame.pack(fill=BOTH, expand=YES)
-        self.createScreenCanvas()
+        self.create_screen_canvas()
 
     def OCR(self):
-        
+
         pytesseract.pytesseract.tesseract_cmd = (
             r"C:\Program Files\Tesseract-OCR\tesseract.exe"
         )
@@ -52,23 +40,21 @@ class Application:
         subprocess.run(["clip.exe"], input=txt.strip().encode("utf-16"), check=True)
         exit()
 
-    def takeBoundedScreenShot(self, x1, y1, x2, y2):
+    def take_bounded_screenshot(self, x1, y1, x2, y2):
         im = pyautogui.screenshot(region=(x1, y1, x2, y2))
-        x = datetime.datetime.now()
-        fileName = x.strftime("%f")
         im.save("sniptoclip.png")
         self.OCR()
 
-    def createScreenCanvas(self):
+    def create_screen_canvas(self):
         self.master_screen.deiconify()
         root.withdraw()
 
-        self.screenCanvas = Canvas(self.picture_frame, cursor="cross", bg="grey11")
-        self.screenCanvas.pack(fill=BOTH, expand=YES)
+        self.screen_canvas = Canvas(self.picture_frame, cursor="cross", bg="grey11")
+        self.screen_canvas.pack(fill=BOTH, expand=YES)
 
-        self.screenCanvas.bind("<ButtonPress-1>", self.on_button_press)
-        self.screenCanvas.bind("<B1-Motion>", self.on_move_press)
-        self.screenCanvas.bind("<ButtonRelease-1>", self.on_button_release)
+        self.screen_canvas.bind("<ButtonPress-1>", self.on_button_press)
+        self.screen_canvas.bind("<B1-Motion>", self.on_move_press)
+        self.screen_canvas.bind("<ButtonRelease-1>", self.on_button_release)
 
         self.master_screen.attributes("-fullscreen", True)
         self.master_screen.attributes("-alpha", 0.3)
@@ -76,11 +62,11 @@ class Application:
         self.master_screen.attributes("-topmost", True)
 
     def on_button_release(self, event):
-        self.recPosition()
+        self.rec_position()
 
         if self.start_x <= self.curX and self.start_y <= self.curY:
             print("right down")
-            self.takeBoundedScreenShot(
+            self.take_bounded_screenshot(
                 self.start_x,
                 self.start_y,
                 self.curX - self.start_x,
@@ -89,7 +75,7 @@ class Application:
 
         elif self.start_x >= self.curX and self.start_y <= self.curY:
             print("left down")
-            self.takeBoundedScreenShot(
+            self.take_bounded_screenshot(
                 self.curX,
                 self.start_y,
                 self.start_x - self.curX,
@@ -98,7 +84,7 @@ class Application:
 
         elif self.start_x <= self.curX and self.start_y >= self.curY:
             print("right up")
-            self.takeBoundedScreenShot(
+            self.take_bounded_screenshot(
                 self.start_x,
                 self.curY,
                 self.curX - self.start_x,
@@ -107,16 +93,16 @@ class Application:
 
         elif self.start_x >= self.curX and self.start_y >= self.curY:
             print("left up")
-            self.takeBoundedScreenShot(
+            self.take_bounded_screenshot(
                 self.curX, self.curY, self.start_x - self.curX, self.start_y - self.curY
             )
 
-        self.exitScreenshotMode()
+        self.exit_screenshot_mode()
         return event
 
-    def exitScreenshotMode(self):
+    def exit_screenshot_mode(self):
         print("Screenshot mode exited")
-        self.screenCanvas.destroy()
+        self.screen_canvas.destroy()
         self.master_screen.withdraw()
         root.deiconify()
 
@@ -126,21 +112,21 @@ class Application:
 
     def on_button_press(self, event):
         # save mouse drag start position
-        self.start_x = self.screenCanvas.canvasx(event.x)
-        self.start_y = self.screenCanvas.canvasy(event.y)
+        self.start_x = self.screen_canvas.canvasx(event.x)
+        self.start_y = self.screen_canvas.canvasy(event.y)
 
-        self.rect = self.screenCanvas.create_rectangle(
+        self.rect = self.screen_canvas.create_rectangle(
             self.x, self.y, 1, 1, outline="red", width=3, fill="blue"
         )
 
     def on_move_press(self, event):
         self.curX, self.curY = (event.x, event.y)
         # expand rectangle as you drag the mouse
-        self.screenCanvas.coords(
+        self.screen_canvas.coords(
             self.rect, self.start_x, self.start_y, self.curX, self.curY
         )
 
-    def recPosition(self):
+    def rec_position(self):
         print(self.start_x)
         print(self.start_y)
         print(self.curX)
